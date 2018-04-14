43df41c93cc2fe089fca41d105b2a13fe439f840
Weetabixx <robingraf@hotmail.co.uk>
2018-04-14 18:39:08 +0100

    completed tests

--- test.sh
+++ test.sh
@@ -1,33 +1,32 @@
 	#!/bin/bash
 	
 	
-	#worlds=(grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2) 
-	worlds=(freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box 
-		freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 
-		freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 
-		freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 
-		freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 
-		freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 
-		freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 
-		grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book 
-		grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 
-		grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 
-		grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 
-		grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 
-		grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 
-		grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 
-		grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 ) 
+	#worlds=(grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2
+		#freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box 
+		#freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 
+		#freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 
+		#freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 
+		#freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 
+		#freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 
+		#freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 ) 
+	worlds=(grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book grabbing_book 
+		grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 grabbing_book2 
+		grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 grabbing_book3 
+		grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 grabbing_book4 
+		grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 grabbing_book5 
+		grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 grabbing_book6 
+		grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 grabbing_book7 
+		grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 grabbing_book8 ) 
 
 	
-	#experiments=(book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf )
-	experiments=(freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box 
-		freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 
-		freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 
-		freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 
-		freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 
-		freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 
-		freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 
-		book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf 
+	#experiments=(book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box freezer_box 
+		#freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 freezer_box2 
+		#freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 freezer_box3 
+		#freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 freezer_box4 
+		#freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 freezer_box5 
+		#freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 freezer_box6 
+		#freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 freezer_box7 )
+	experiments=(book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf book_on_shelf 
 		book_on_shelf2 book_on_shelf2 book_on_shelf2 book_on_shelf2 book_on_shelf2 book_on_shelf2 book_on_shelf2 book_on_shelf2 book_on_shelf2 book_on_shelf2 book_on_shelf2 
 		book_on_shelf3 book_on_shelf3 book_on_shelf3 book_on_shelf3 book_on_shelf3 book_on_shelf3 book_on_shelf3 book_on_shelf3 book_on_shelf3 book_on_shelf3 book_on_shelf3 
 		book_on_shelf4 book_on_shelf4 book_on_shelf4 book_on_shelf4 book_on_shelf4 book_on_shelf4 book_on_shelf4 book_on_shelf4 book_on_shelf4 book_on_shelf4 book_on_shelf4 
@@ -38,7 +37,7 @@
 	index=0
 
 
-	while [ $index -lt 150 ]; do
+	while [ $index -lt 88 ]; do
 
 	gnome-terminal -e "timeout 180s roslaunch skill_transfer simulation.launch world:=${worlds[$index]}" #kill node?
 	sleep 10s;
@@ -46,8 +45,8 @@
 	sleep 190;
 	
 	echo -e " $index : \n" >>output.txt
-	echo -e " ${worlds[$index]} : \n" >>output.txt	
-	echo -e " ${experiments[$index]} : \n" >>output.txt
+	echo -e " ${worlds[$index]} : \n" >>bookoutput.txt	
+	echo -e " ${experiments[$index]} : \n" >>bookoutput.txt
 	while read -r row; do
 	echo -e "$row \n" >>output.txt #depends on the format
 	done < ~/.gazebo/server-11345/default.log
